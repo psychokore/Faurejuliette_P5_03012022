d5af0e94dedcee75414e7cb15e91bcce01357870
psychokore <juju.faure@yahoo.fr>
2022-01-09 15:48:06 +0100

    correctifs mentorat

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,19 @@
 //Récupération de l'ID//
-let url = new URL(window.location.href);
-let id = url.searchParams("id");
+const url = new URLSearchParams(window.location.search); //récupération des query au sein de l'URL
+
+if (!url.has("id")) {
+    window.location.href = "http://localhost:5500/front/html/index.html" //redirection à la page d'acceuil si non existant
+}
+
+fetch(`http://localhost:3000/api/products/${url.get("id")}`)
+.then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+.then(function (product){
+    if (!product){
+        window.location.href = "http://localhost:5500/front/html/index.html" //redirection à la page d'acceuil si non existant  
+    }
+})
+.catch(error=>window.location.href = "http://localhost:5500/front/html/index.html")
