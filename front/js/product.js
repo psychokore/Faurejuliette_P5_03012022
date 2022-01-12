76645977c90240a11e692a0cf157ba6a164aec95
psychokore <juju.faure@yahoo.fr>
2022-01-12 13:51:35 +0100

    product page setup

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,6 @@
 //Récupération de l'ID//
 const url = new URLSearchParams(window.location.search); //récupération des query au sein de l'URL
+//let id = url.get("id");
 
 if (!url.has("id")) {
     window.location.href = "http://localhost:5500/front/html/index.html" //redirection à la page d'acceuil si non existant
@@ -17,3 +18,21 @@ fetch(`http://localhost:3000/api/products/${url.get("id")}`)
     }
 })
 .catch(error=>window.location.href = "http://localhost:5500/front/html/index.html")
+.then ((data) => pageContent(data));
+
+
+//Création des variables
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const colors = document.getElementById("colors");
+const image = document.getElementsByClassName("item__img")[0];
+
+//Affichage du produit
+function pageContent(products){
+    for (let i = 0; i < products.length; i++) {
+        title.innerHTML = products[i].name;
+        price.innerHTML = products[i].price;
+        description.innerHTML = products[i].description;
+    }
+}
